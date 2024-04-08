cf59c507aeafeb091109b1ac3e3035ee9390bc58
Vishal7410 <vishalpardhi2001@gmail.com>
2024-04-08 23:48:45 +0530

    initial SetUp of App

---
+++ README.md
@@ -0,0 +1,21 @@
+- Building the YOUTUBE App
+
+Requirement Clarification
+ Feature
+ Tech Stack
+ - Redux
+ - Tailwind
+
+Planning
+
+        Head
+         Body
+        Sidebar
+           MenuItem
+        MainContainer
+        - ButtonList
+        - VideoContainer
+        -  VideoCard
+      
+        
+  
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Body from './components/Body'
+import Header from './components/Header'
+import SideBar from './components/SideBar'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div>
+  
+      <Header/>
+      <Body/>
+   
+      
+    </div>
+      
+    </>
+  )
+}
+
+export default App
